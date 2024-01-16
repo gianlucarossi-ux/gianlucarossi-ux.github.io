7461bb8727cd530ea17d50e96727ebd1c2dd8401
gianlucarossi-ux <rossi.gnlc@gmail.com>
2024-01-16 18:48:23 +0100

    Version 2.1
    New hero section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var typed = new Typed('#mission', {
+/*var typed = new Typed('#mission', {
   strings: ['Progetto soluzioni di valore a partire dai bisogni degli utenti'],
   typeSpeed: 50,
   backSpeed: 50,
@@ -12,9 +12,16 @@ var typed = new Typed('#mission2', {
   backSpeed: 50,
   backDelay: 5000,
   loop: true
-});
+});*/
 
 AOS.init({
   once: true, //per eseguire le animazioni solo una volta
   duration: 1000, //durata dell'animazione
 });
+
+//funzione per chiudere il menu al click su dispositivi mobile
+function closeNavbar() {
+  if (window.innerWidth <= 992) {
+    $('.navbar-toggler').click();
+  }
+}
