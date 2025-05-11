ec3bee63eac6523a6dc8d02e19250548f867612a
gianlucarossi-ux <rossi.gnlc@gmail.com>
2025-05-11 19:15:18 +0200

    V3.1
    Complete website

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// Inizializza AOS
+// Inizializzazione AOS
 AOS.init({
-    duration: 800, // Durata animazione (in millisecondi)
+    duration: 800, // Durata animazione
     easing: 'ease-in-out', // Easing per la transizione
-    once: true, // Animazione eseguita solo una volta
+    once: true, // Esegue l'animazione solo al primo caricamento
 });
+
+// Funzione per stampare l'anno corrente nel footer
+document.addEventListener('DOMContentLoaded', () => {
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
+});
+  
